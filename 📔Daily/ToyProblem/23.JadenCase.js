a518855d6be4232607c99cd946e3b7f9811d0667
YounglanHong <yong2401@gmail.com>
2020-05-27 11:09:17 +0900

    Modify daily toy problem

--- 📔Daily/ToyProblem/23.JadenCase.js
+++ 📔Daily/ToyProblem/23.JadenCase.js
@@ -4,6 +4,7 @@
 /* 블로깅 주소: https://www.notion.so/5-28-8d58907af4224283bc5a45cf4f016374 */
 
 // 풀이1
+//* 정규표현식
 function solution(s) {
   /* s의 첫글자 영문이면, 대문자/ 영문 아니면, 그대로 */
 
@@ -24,5 +25,20 @@ function solution(s) {
   });
 
   answer = words.join("");
+  return answer;
+}
+
+// 풀이2
+//* toUpperCase(), toUpperCase(): 문자열 아닌 글자는 적용 안됨
+function solution(s) {
+  var answer = "";
+  // [조건] 문자열의 첫글자 또는 공백 다음 글자는 대문자
+  for (let i = 0; i < s.length; i++) {
+    if (i === 0 || s[i - 1] === " ") {
+      answer += s[i].toUpperCase();
+    } else {
+      answer += s[i].toLowerCase();
+    }
+  }
   return answer;
 }

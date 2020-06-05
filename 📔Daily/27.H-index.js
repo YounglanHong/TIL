7267326d2bbf636041078899147bf7fdf1d63689
YounglanHong <yong2401@gmail.com>
2020-06-05 23:59:12 +0900

    Update daily toy problem

--- 📔Daily/27.H-index.js
+++ 📔Daily/27.H-index.js
@@ -2,6 +2,8 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42747
 
 function solution(citations) {
+  /* [조건] h 지수는 그 사람이 쓴 모든 논문 중 n회 이상 인용된 논문이 
+      n개 이상일 때, 이 둘을 동시에 만족하는 n의 최대값 */
   var answer = 0;
   let length = citations.length;
   let array = [];
